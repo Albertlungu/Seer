1e1bb8e2a1d5c3c2132f049472721c488c353953
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2026-02-21 15:56:14 -0500

    FEAT: Make the Open3D scene creation function as well as the actual raycasting logic and integrate them.

--- src/video_processing/material_tagging/raycasting.py
+++ src/video_processing/material_tagging/raycasting.py
@@ -7,6 +7,7 @@ The full raycasting pipeline, including the camera pose calculator.
 from pathlib import Path
 
 import numpy as np
+import open3d as o3d
 import pycolmap
 
 database_path = Path("./data/env_imgs/colmap_albert_room/database.db")
@@ -26,8 +27,12 @@ class Raycast:
         t: np.ndarray,
         image_w: int,
         image_h: int,
+        obj_path: str,
+        origin: np.ndarray,
+        direction: np.ndarray,
     ):
-        """_summary_
+        """
+        Initializing the Raycasting class.
 
         Args:
             database_path (str): Path to COLMAP database file
@@ -39,6 +44,9 @@ class Raycast:
             t (np.ndarray): Translation vector (3x1 array)
             image_w (int): Width of the image in pixels
             image_h (int): Height of the image in pixels
+            obj_path (str): Path to the OBJ mesh file.
+            origin: (np.ndarray): 3D coordinates of the ray origin
+            direction (np.ndarray): 3D vector for the ray direction
         """
         self.bbox = bbox
         self.K = K
@@ -49,6 +57,8 @@ class Raycast:
         self.db_path = database_path
         self.img_path = image_path
         self.output_path = output_path
+        self.obj_path = obj_path
+        self.origin = origin
 
     def camera_pose(self) -> dict:
         """
@@ -108,3 +118,37 @@ class Raycast:
             rays.append((origin, ray_world))
 
         return rays
+
+    def setup_scene(self) -> o3d.t.geometry.RaycastingScene:
+        """
+        Loads the OBJ mesh and sets up the Open3D Raycasting Scene
+
+        Returns:
+            o3d.t.geometry.RaycastingScene: Open3D raycasting scene with the mesh loaded.
+        """
+        mesh_o3d = o3d.io.read_triangle_mesh(self.obj_path)
+        mesh_t = o3d.t.geometry.TriangleMesh.from_legacy(mesh_o3d)
+        scene = o3d.t.geometry.RaycastingScene()
+        scene.add_triangles(mesh_t)
+        return scene
+
+    def raycast(self) -> list | None:
+        """
+        Main raycasting logic.
+
+        Returns:
+            list | None: list of 3D hit points on mesh or None if all rays.
+        """
+        rays = self.unprojection()
+        scene = self.setup_scene()
+        hit_points = []
+
+        for origin, direction in rays:
+            ray = o3d.core.Tensor([[*origin, *direction]], dtype=o3d.core.float32)
+            result = scene.cast_rays(ray)
+            t_hit = result["t_hit"].numpy()[0]
+            if np.isinf(t_hit):
+                hit_points.append(None)
+            else:
+                hit_points.append(np.array(origin) + t_hit * np.array(direction))
+        return hit_points
