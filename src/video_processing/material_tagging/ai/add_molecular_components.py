1d6a48a777436862f41bc36aa09a66d870504abb
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2026-03-19 13:34:50 -0400

    REFACTOR: Make legacy folders for all old vision loaders and whatnot

---
+++ src/video_processing/material_tagging/ai/add_molecular_components.py
@@ -0,0 +1,6 @@
+"""
+./src/video_processing/material_tagging/ai/add_molecular_components.py
+
+Takes the annotations.json file, reads the materials for each object, installs a local Ollama model,
+adds the molecular composition for each material to the same json, then removes the Ollama model.
+"""
